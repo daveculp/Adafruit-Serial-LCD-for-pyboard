43da6cbe9ecf564cea428f57d65b1e942e3039d8
daveculp <dwculp@gmail.com>
2016-07-19 21:47:20 -0500

    Added copyright notice

--- afserlcd.py
+++ afserlcd.py
@@ -1,6 +1,24 @@
+# Copyright 2016 David Culp (dwculp@gmail.com)
+#
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+#This is a simple class for interfacing an Adafruit serial LCD with a pyboard.
+
 import pyb
 
-#define our commands
+#define our LCD commands
 LCD_COMMAND             =   0xFE
 DISPLAY_ON              =   0x42 
 DISPLAY_OFF             =   0x46
